a37bd66cb924150a07e37f4078f137479da5840d
laudebugs <lbugasu@gmail.com>
2023-02-04 09:39:28 -0500

    fix: theming + font

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { HelperService } from './core/helper.service'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  #collapsed = JSON.parse(localStorage.getItem('collapsed') ?? 'false')
-
-  get collapsed(): boolean {
-      return this.#collapsed
-  }
-  set collapsed(value: boolean) {
-      this.#collapsed = value
-      localStorage.setItem('collapsed', JSON.stringify(value))
-  }
-
-  toggleCollapse(): void {
-      this.collapsed = !this.collapsed
-  }
+    constructor(public helperService: HelperService) {}
 }
---
+++ src/app/core/helper.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { NavigationEnd, Router } from '@angular/router'
+import { filter, map, tap } from 'rxjs'
+
+@Injectable({
+    providedIn: 'root',
+})
+export class HelperService {
+    #collapsed = JSON.parse(localStorage.getItem('collapsed') ?? 'false')
+
+    get collapsed(): boolean {
+        return this.#collapsed
+    }
+    set collapsed(value: boolean) {
+        this.#collapsed = value
+        localStorage.setItem('collapsed', JSON.stringify(value))
+    }
+
+    currentRoute = '/'
+
+    constructor(private snackBarSvc: MatSnackBar, private router: Router) {
+        this.currentRoute = this.router.url
+        this.router.events
+            .pipe(
+                filter((event) => event instanceof NavigationEnd),
+                map((event) => (event as NavigationEnd).url),
+                tap((url) => (this.currentRoute = url)),
+            )
+            .subscribe()
+    }
+
+    showSnackBar(message: string, action: string, duration: number): void {
+        this.snackBarSvc.open(message, action, { duration })
+    }
+
+    toggleCollapse(): void {
+        this.collapsed = !this.collapsed
+    }
+}
